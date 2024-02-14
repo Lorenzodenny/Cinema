96697f41ee7f6ddc36056261d922bb7b5578a46b
“Marco” <marcantonio1963@libero.it>
2024-02-14 16:59:10 +0100

    Aggiungere i file di progetto.

---
+++ AddClient.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddClient.aspx.cs" Inherits="Cinema.AddClient" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblNome" runat="server" Text="">Nome</asp:Label>
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="lblCognome" runat="server" Text="">Cognome</asp:Label>
+            <asp:TextBox ID="txtCognome" runat="server"></asp:TextBox>
+            <br />
+            Sala:
+            <asp:RadioButtonList ID="rdbSala" runat="server">
+                <asp:ListItem Value="nord" >Sala Nord</asp:ListItem>
+                <asp:ListItem Value="est" >Sala Est</asp:ListItem>
+                <asp:ListItem Value="sud" >Sala Sud</asp:ListItem>
+            </asp:RadioButtonList>
+            <br />
+            Tipo biglietto:
+            <asp:RadioButtonList ID="rdbTipoBiglietto" runat="server">
+                <asp:ListItem Value="ridotto" >Ridotto</asp:ListItem>
+                <asp:ListItem Value="intero" >Intero</asp:ListItem>
+            </asp:RadioButtonList>
+
+            <asp:Button ID="btnAcquista" runat="server" Text="Acquista" OnClick="btnAcquista_Click" />
+            
+            <div>
+                Sala Nord:<asp:Label ID="lblNord" runat="server" Text="Label"></asp:Label>
+                <br />
+                Sala Est<asp:Label ID="lblEst" runat="server" Text="Label"></asp:Label>
+                <br />
+                Sala Sud<asp:Label ID="lblSud" runat="server" Text="Label"></asp:Label>
+                <br />
+                Totale Ridotti<asp:Label ID="lblRidotti" runat="server" Text="Label"></asp:Label>
+            </div>
+
+
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ AddClient.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace Cinema
+{
+    public partial class AddClient : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAcquista_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["BDCinemaConnetion"].ToString();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string nome = txtName.Text;
+                string cognome = txtCognome.Text;
+                string sala = rdbSala.Text;
+                string tipoBiglietto = rdbTipoBiglietto.Text;
+
+                int countNord = GetCount(conn, "nord");
+                int countEst = GetCount(conn, "est");
+                int countSud = GetCount(conn, "sud");
+                int countSalaSelected = GetCount(conn,sala);
+                
+
+                using (SqlCommand insertClient = new SqlCommand("Insert Into Utenti (Nome,Cognome,Sala,TipoBiglietto) values (@nome, @cognome, @sala, @tipoBiglietto)", conn))
+
+                {
+                    insertClient.Parameters.AddWithValue("@nome", nome);
+                    insertClient.Parameters.AddWithValue("@cognome", cognome);
+                    insertClient.Parameters.AddWithValue("@sala", sala);
+                    insertClient.Parameters.AddWithValue("@tipoBiglietto", tipoBiglietto);
+
+                    int affectedRow = insertClient.ExecuteNonQuery();
+                    if (affectedRow != 0)
+                    {
+                        Response.Write("Biglietto acquistato");
+                    }
+                    else
+                    {
+                        Response.Write("Acquisto non riuscito");
+                    }
+                }
+            }
+        }
+
+        private int GetCount(SqlConnection conn, string sala)
+        {
+            using (SqlCommand countSala = new SqlCommand("select count(*) from Utenti where Sala=@sala", conn))
+            {
+                countSala.Parameters.AddWithValue("@sala", sala);
+                return (int)countSala.ExecuteScalar();
+            }
+        }
+    }
+}
